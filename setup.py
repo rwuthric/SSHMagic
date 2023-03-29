1f5158d0c23bf6f36d131c5ae1a8e022ad66b0c9
Rolf Wuthrich <rolf.wuthrich@concordia.ca>
2023-03-29 19:19:44 -0400

    Corrected url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sshmagic',
     version='0.1.0',    
     description='IPython magic for executing commands on a remote machine via ssh',
-    url='https://github.com/rwuthric/SSHMagice',
+    url='https://github.com/rwuthric/SSHMagic',
     author='Rolf Wuthrich',
     author_email='rolf.wuthrich@concordia.ca',
     license='BSD 3-Clause License',
